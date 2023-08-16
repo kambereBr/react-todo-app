1782058dfbe1e7447d48d37b06bde48b816f1919
kambereBr <brunokambere@gmail.com>
2023-08-16 18:38:33 +0200

    added ToDoList.js to components

---
+++ src/components/ToDoList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ToDoItem from './ToDoItem';
+
+/* eslint-disable max-len */
+const ToDoList = ({
+  todosProps, handleChange, delTodo, setUpdate,
+}) => (
+  <ul>
+    {todosProps.map((todo) => (
+      <ToDoItem key={todo.id} itemProp={todo} handleChange={handleChange} delTodo={delTodo} setUpdate={setUpdate} />
+    ))}
+  </ul>
+);
+
+ToDoList.propTypes = {
+  todosProps: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+  })).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+};
+
+export default ToDoList;
